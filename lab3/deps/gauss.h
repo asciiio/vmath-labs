6f792623a212c170e478c23d605cf1321a055824
asciiio <126962074+asciiio@users.noreply.github.com>
2023-10-09 21:17:37 +0300

    08.10.23

---
+++ lab3/deps/gauss.h
@@ -0,0 +1,10 @@
+#ifndef GAUSS_H
+#define GAUSS_H
+
+#include <stdlib.h>
+#include "matrix.h"
+
+double* gaussMethod(int, double **);
+double *solutionCheck(int, double**, double*);
+
+#endif // GAUSS_H
---
+++ lab3/least.h
@@ -0,0 +1,10 @@
+#ifndef LEAST_H
+#define LEAST_H
+
+#include "deps/vector.h"
+#include "deps/gauss.h"
+#include <math.h>
+
+double* least(vect_t vcts, uint16_t pol_pow);
+
+#endif /* LEAST_H */
